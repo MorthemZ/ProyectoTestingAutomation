e14272c1dc422b7d60e57a108ea07cb19ee6a397
Matias Mendez <elcuervomendez@gmail.com>
2025-10-24 22:55:54 -0300

    inicio del proyecto

---
+++ tests/test_login.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_login_validation(login_in_driver):
+    try:
+        driver = login_in_driver
+
+        assert "/inventory.html" in driver.current_url, "No se redirigio al inventario"
+
+    except Exception as e:
+        print(f"Error en  test_login: {e}")
+        raise
+    finally:
+        driver.quit()
