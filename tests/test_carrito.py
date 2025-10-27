748ae4f890dcee41812fbb9e10acaea3e88789e6
Matias Mendez <elcuervomendez@gmail.com>
2025-10-27 20:52:24 -0300

    Agrego validaciones de inventario y primer producto agregado un detalle

--- tests/test_carrito.py
+++ tests/test_carrito.py
@@ -9,7 +9,9 @@ def test_add_cart(login_in_driver):
         
 # Hacer click en "Agregar al carrito"
         driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-#driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click() / este esta para ver que pasaba si agregaba mas de uno /
+#Tambien puedo usar esta para buscar el primer elemento con css_selector en vez del id si borrasen el mismo
+        #driver.find_element(By.CSS_SELECTOR, ".inventory_item button").click()
+        #driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click() / este esta para ver que pasaba si agregaba mas de uno /
 # Espera explícita: hasta que el carrito tenga al menos un elemento visible
         wait = WebDriverWait(driver, 5)  # espera hasta 5 segundos
         wait.until(EC.presence_of_element_located((By.CLASS_NAME, "shopping_cart_link")))
